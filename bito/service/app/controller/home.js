4ed11981542d9dc6f4048473a7bc384be26050f4
hemengting <luna630h@163.com>
2021-11-30 22:53:42 +0800

    feat:添加路由

--- bito/service/app/controller/home.js
+++ bito/service/app/controller/home.js
@@ -7,6 +7,10 @@ class HomeController extends Controller {
     const { ctx } = this;
     ctx.body = 'hi, egg';
   }
+  async list() {
+    const { ctx } = this;
+    ctx.body = '<h1>hi,hmt</h1>';
+  }
 }
 
 module.exports = HomeController;

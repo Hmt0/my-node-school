33690d9779daa7ae8dedc0cd170f610ac7a6dad7
hemengting <luna630h@163.com>
2021-12-22 01:13:41 +0800

    feat: 后台管理界面

--- bito/admin/src/Pages/Login.js
+++ bito/admin/src/Pages/Login.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
+import 'antd/dist/antd.css'
+import {Card, Input, Button, Spin} from 'antd'
+import '../static/Login.css'
 
 function Login() {
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoading, setIsloading] = useState(false)
+
+    const checkLogin = () => {
+        setIsloading(true)
+        setTimeout(() => {
+            setIsloading(false)
+        }, 1000)
+    }
+
     return (
-        <div>
-            登录
+        <div className="login-div">
+            <Spin tip='Loading...' spinning={isLoading}>
+                <Card title="HMTBlog System" bordered={true} style={{width: 400}}>
+                    <Input 
+                        id="userName"
+                        size="large"
+                        placeholder="Enter your userName"
+                        // prefix={<Icon type="user" style={{color: 'rgba(0, 0, 0, .25)'}} />}
+                        onChange={(e) => {setUserName(e.target.value)}}
+                    />
+                    <br /><br/>
+                    <Input 
+                        id="password"
+                        size="large"
+                        placeholder="Enter your password"
+                        type="key"
+                        // prefix={<Icon type="key" style={{color: 'rgba(0, 0, 0, .25)'}} />}
+                        onChange={(e) => {setPassword(e.target.value)}}
+                    />
+                    <br /><br/>
+                    <Button type="primary" size="large" block onClick={checkLogin}>login</Button>
+                </Card>
+            </Spin>
         </div>
     )
 }
--- bito/admin/src/Pages/Main.js
+++ bito/admin/src/Pages/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import AdminIndex from "./AdminIndex";
 import Login from "./Login";
 
 function Main() {
@@ -7,6 +8,7 @@ function Main() {
         <Router>
             <Routes>
                 <Route path="/login/" exact element={<Login />}></Route>
+                <Route path="/admin/" exact element={<AdminIndex />}></Route>
             </Routes>
             
         </Router>
